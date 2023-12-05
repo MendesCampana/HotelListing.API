fecb18ef9612386fb4fab84415b9466bea904bdb
Mykhailo Marchenko <mykhailom@qognify.com>
2023-12-05 15:41:57 +0200

    Add DB migrations for default roles

---
+++ HotelListing.API/Migrations/20231205134024_AddedDefaultRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "00a3eaa6-d34a-4b4d-aed6-fc33aee10a4a", null, "Administrator", "ADMINISTRATOR" },
+                    { "747d7b10-d819-443d-887f-0990435c268d", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "00a3eaa6-d34a-4b4d-aed6-fc33aee10a4a");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "747d7b10-d819-443d-887f-0990435c268d");
+        }
+    }
+}
