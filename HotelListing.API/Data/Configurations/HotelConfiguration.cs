e7447f29c955bd4c10c43bb03ac5b957e5672d48
Mykhailo Marchenko <mykhailom@qognify.com>
2023-12-05 15:38:02 +0200

    Add identity migrations Add data configurations for DB seeding

---
+++ HotelListing.API/Data/Configurations/HotelConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configurations
+{
+    public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+    {
+        public void Configure(EntityTypeBuilder<Hotel> builder)
+        {
+            builder.HasData([
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sunrise Beach",
+                    Address = "RedSeaBeach, 101",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Grand Paladium",
+                    Address = "Caymann 201",
+                    CountryId = 2,
+                    Rating = 3.5
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Small Paladium",
+                    Address = "Zizzics 11",
+                    CountryId = 3,
+                    Rating = 5
+                },
+                new Hotel
+                {
+                    Id = 4,
+                    Name = "Trash Palace",
+                    Address = "Samps 666",
+                    CountryId = 1,
+                    Rating = 2
+                }]);
+        }
+    }
+}
--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using HotelListing.API.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Data
 {
-    public class HotelListingDbContext : DbContext
+    public class HotelListingDbContext : IdentityDbContext<ApiUser>
     {
         public HotelListingDbContext(DbContextOptions options) : base(options)
         {
@@ -13,59 +15,11 @@ namespace HotelListing.API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Country>().HasData([          
-                new Country
-                {
-                    Id = 1,
-                    Name = "Jamaica",
-                    ShortName = "JM"
-                },
-                new Country
-                {
-                    Id = 2,
-                    Name = "Bahamas",
-                    ShortName = "BS"
-                },
-                new Country
-                {
-                    Id = 3,
-                    Name = "Ukraine",
-                    ShortName = "UKR"
-                }]);
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
 
-            modelBuilder.Entity<Hotel>().HasData([
-                new Hotel
-                {
-                    Id = 1,
-                    Name = "Sunrise Beach",
-                    Address = "RedSeaBeach, 101",
-                    CountryId = 1,
-                    Rating = 4.5
-                },
-                new Hotel
-                {
-                    Id = 2,
-                    Name = "Grand Paladium",
-                    Address = "Caymann 201",
-                    CountryId = 2,
-                    Rating = 3.5
-                }, 
-                new Hotel
-                {
-                    Id = 3,
-                    Name = "Small Paladium",
-                    Address = "Zizzics 11",
-                    CountryId = 3,
-                    Rating = 5
-                },
-                new Hotel
-                {
-                    Id = 4,
-                    Name = "Trash Palace",
-                    Address = "Samps 666",
-                    CountryId = 1,
-                    Rating = 2
-                }]);
+            modelBuilder.ApplyConfiguration(new CountryConfiguration());
+
+            modelBuilder.ApplyConfiguration(new HotelConfiguration());
         }
     }
 }
